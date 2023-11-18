2671ff7101af8b73563cacf785d50f041e0f98a1
gaurav1021 <78587810+gaurav1021@users.noreply.github.com>
2023-11-19 04:34:41 +0530

    Update quick_sort.js

--- scripts/quick_sort.js
+++ scripts/quick_sort.js
@@ -14,7 +14,7 @@ function Quick() {
   //Setting Space complexity
   document.getElementById("Space_Worst").innerText = "O(log N)";
 
-  fetch("../Sorting_Visualizer/scripts/info_qck.html")
+  fetch("../sorting-visualizer/scripts/info_qck.html")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
